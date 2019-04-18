f601970491ef8e6edbaa92f62f3e970c465489ce
2529716798 <2529716798@qq.com>
2019-04-19 01:22:58 +0800

    Finished

--- country.R
+++ country.R
@@ -4,12 +4,48 @@ library(frontier)
 library(gdata)
 data(front41Data)
 data <- read.xls("data.xlsx", na.strings=c("NA","#DIV/0!"), sheet="Sheet1")
-print(data)
-print("------\n-------\n------")
-print(front41Data)
+data2 <- read.xls("data.xlsx", na.strings=c("NA","#DIV/0!"), sheet="Sheet2")
+data3 <- read.xls("data.xlsx", na.strings=c("NA","#DIV/0!"), sheet="Sheet3")
+data4 <- read.xls("data.xlsx", na.strings=c("NA","#DIV/0!"), sheet="Sheet4")
+# sfa_GDP_another <- function(sheet_num, header1, header2) {
+#    print("---")
+#    sheet <- paste("Sheet", sheet_num, sep="")
+#    print(sheet)
+#    data <- read.xls("data.xlsx", na.strings=c("NA","#DIV/0!"), sheet="Sheet1")
+#    sfaResult <- sfa( log( GDP ) ~ log( COR ) + log( EFF ),
+#           data = data )
+#    coef( sfaResult, which = "ols" )
+#    coef( sfaResult, which = "grid" )
+#    coef( sfaResult)
+#    print("-------Efficiencies-------")
+#    efficiencies(sfaResult)
 
- sfaResult <- sfa( log( GDP ) ~ log( COR ) + log( EFF ),
-          data = data )
-       coef( sfaResult, which = "ols" )
-       coef( sfaResult, which = "grid" )
-       coef( sfaResult )
+# }
+
+print("----Data1----")
+sfaResult <- sfa( log( GDP ) ~ log( COR ) + log( EFF ), data = data )
+coef( sfaResult, which = "ols" )
+coef( sfaResult, which = "grid" )
+coef( sfaResult )
+
+print("----Data2----")
+sfaResult <- sfa( log( GDP ) ~ log( REG ) + log( LAW ), data = data2 )
+coef( sfaResult, which = "ols" )
+coef( sfaResult, which = "grid" )
+coef( sfaResult )
+
+print("----Data3----")
+sfaResult <- sfa( log( GDP ) ~ log( VOL ) + log( STA ), data = data3 )
+coef( sfaResult, which = "ols" )
+coef( sfaResult, which = "grid" )
+coef( sfaResult )
+
+sfaResult <- sfa( log( GDP ) ~ log( LAO ) + log( GDPR ), data = data4 )
+coef( sfaResult, which = "ols" )
+coef( sfaResult, which = "grid" )
+coef( sfaResult )
+
+
+
+
+
